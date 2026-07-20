192c9aa9c4f7c2917adfb5e66dc6a98e50aeb7b2
Neel0289 <neel9086@gmail.com>
2026-07-20 15:18:36 +0530

    2 lectures completed

---
+++ lec2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int  main() {
+    int score = 100;
+    cout << "Score is: " << score << endl;
+    cout << "Welcome bro" << endl;
+    
+    return 0;
+}
